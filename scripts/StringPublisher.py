55d288dd45b79d59e22667033d9af9c0044a6f8f
Michdo93 <michaeldoerflinger93@gmail.com>
2022-08-11 10:38:39 +0200

    Update StringPublisher.py

--- scripts/StringPublisher.py
+++ scripts/StringPublisher.py
@@ -13,7 +13,7 @@ class StringPublisher(object):
 
         # Initialize message variables.
         self.enable = False
-        self.message = ""
+        self.message = None
 
         if self.enable:
             self.start()
