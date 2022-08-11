664069441355067ab8aa20a0edd2d90f4a82b43f
Michdo93 <michaeldoerflinger93@gmail.com>
2022-08-11 10:37:16 +0200

    Update DimmerPublisher.py

--- scripts/DimmerPublisher.py
+++ scripts/DimmerPublisher.py
@@ -13,7 +13,7 @@ class DimmerPublisher(object):
 
         # Initialize message variables.
         self.enable = False
-        self.message = ""
+        self.message = None
 
         if self.enable:
             self.start()
