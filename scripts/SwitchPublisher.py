37a6eeb7eb61b22407de48f378394d9d8714ad12
Michdo93 <michaeldoerflinger93@gmail.com>
2022-08-11 10:39:00 +0200

    Update SwitchPublisher.py

--- scripts/SwitchPublisher.py
+++ scripts/SwitchPublisher.py
@@ -13,7 +13,7 @@ class SwitchPublisher(object):
 
         # Initialize message variables.
         self.enable = False
-        self.message = ""
+        self.message = None
 
         if self.enable:
             self.start()
